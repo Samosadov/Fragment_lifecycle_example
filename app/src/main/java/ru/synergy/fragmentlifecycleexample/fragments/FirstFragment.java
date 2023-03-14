86f26d877956d5b0584f10563212c02624384957
Samosadov <for-spam2@yandex.ru>
2023-03-14 23:12:38 +0300

    Created the 1st and the 2nd fragments

---
+++ app/src/main/java/ru/synergy/fragmentlifecycleexample/fragments/FirstFragment.java
@@ -0,0 +1,85 @@
+package ru.synergy.fragmentlifecycleexample.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import ru.synergy.fragmentlifecycleexample.R;
+
+public class FirstFragment extends Fragment {
+
+    private static final String TAG = "FRAGMENT2";
+
+    public static FirstFragment newInstance() {
+        FirstFragment fragment = new FirstFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Log.d(TAG, "onCreate()");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate()");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart()");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume()");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause()");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop()");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy()");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d(TAG, "onDetach()");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_first, container, false);
+        TextView tv = rootView.findViewById(R.id.textview);
+        tv.setText(TAG + " layout");
+        Log.d(TAG, "onCreateView()");
+        return rootView;
+    }
+}
