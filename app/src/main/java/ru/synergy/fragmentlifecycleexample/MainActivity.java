f5ad09918eedfc170ef82dde54b1132f63404cba
Samosadov <for-spam2@yandex.ru>
2023-03-14 23:32:23 +0300

    57:00

--- app/src/main/java/ru/synergy/fragmentlifecycleexample/MainActivity.java
+++ app/src/main/java/ru/synergy/fragmentlifecycleexample/MainActivity.java
@@ -3,12 +3,19 @@ package ru.synergy.fragmentlifecycleexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button fragmentButton1, fragmentButton2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentButton1 = (Button) findViewById(R.id.button1);
+        fragmentButton2 = (Button) findViewById(R.id.button2);
+
+
     }
 }
